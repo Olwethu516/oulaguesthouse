d6fa120dd7068f5cf18a70b16445415cd49d2c5a
Olwethu516 <39149618+Olwethu516@users.noreply.github.com>
2026-05-18 12:53:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/oulaguesthouse/', // VERY IMPORTANT
+  base: '/oulasalon/',
+  plugins: [react()],
 })
