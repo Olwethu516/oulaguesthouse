52fdb2a7e0df785e3b178104350f2cddadf84579
Olwethu516 <39149618+Olwethu516@users.noreply.github.com>
2026-05-20 10:02:58 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/oulaguesthouse/',
+  base: '/totalitywellnessspa/',
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
+
